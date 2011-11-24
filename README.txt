b5fd9aba89d66380d594fc30b0a53acc6a503ad2
Andrew Smith <asmith@bcm.com.au>
2011-11-24 17:24:44 +1000

    Initial commit. Not much works yet. Basic checking on form elements for empty values and possible pattern matching (not tested).

---
+++ README.txt
@@ -0,0 +1,6 @@
+Being unable to find a nice HTML5/JS form validator I've decided to hack away at one myself. A lot of external code will
+be used.
+
+* Modernizr - https://github.com/Modernizr/Modernizr
+* jQuery - https://github.com/jquery/jquery
+* qTip2 - https://github.com/craga89/qtip2
---
+++ h5v.js
@@ -0,0 +1,86 @@
+(function($){
+    "use strict";
+
+    $.fn.h5v = function(options) {
+        var defaults = {
+            'required' : !Modernizr.input.required,
+            'pattern'  : !Modernizr.input.pattern,
+            'my': 'left center',
+            'at': 'right center'
+        };
+
+        var opts = $.extend({}, defaults, options);
+
+        this.each(function() {
+            if(opts.required) {
+                $(this).attr('novalidate', 'novalidate');
+
+                $(this).on('submit', function(e) {
+                    $(this).find('input').each(function() {
+                        var self = $(this);
+                        if(validateInput(self) === false) {
+                            self.addClass('error').qtip(
+                                {
+                                    content:
+                                    {
+                                        text: 'Please fill in this field'
+                                    },
+                                    position:
+                                    {
+                                        my: opts.my,
+                                        at: opts.at
+                                    },
+                                    show:
+                                    {
+                                        'event': ''
+                                    },
+                                    hide: 
+                                    {
+                                        'event': 'focusout'
+                                    },
+                                    'events': {
+                                        hide: function(e, api) {
+                                            var isValid = validateInput(api.elements.target);
+                                            if(isValid) {
+                                                api.elements.target.removeClass('error');
+                                            }
+                                            return isValid;
+                                        }
+                                    }
+                                }
+                            ).qtip('show');
+                        }
+                        else
+                        {
+                            self.removeClass('error');
+                        }
+                    });
+
+                    e.preventDefault();
+                    return false;
+                });
+            }
+        });
+
+        function validateInput(input) {
+            var self = input;
+            var val = self.val();
+            var type = self.attr('type');
+            var isValid = true;
+
+            if((type == 'checkbox') || (type == 'radio')) {
+                val = self.is(':checked');
+            }
+
+            var pattern = input.attr('pattern');
+            if(pattern !== undefined) {
+                var regex = new RegExp(pattern);
+                isValid = !!val.match(regex);
+            }
+            else if((input.attr('required') !== undefined) && ((val === "") || (val === false))) {
+                isValid = false;
+            }
+            return isValid;
+        }
+    };
+})(jQuery);
